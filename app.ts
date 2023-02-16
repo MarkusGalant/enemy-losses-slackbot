7c1fc6fd2f0fefdda76c1417f704e480fba76b2c
MarkusGalant <34216383+MarkusGalant@users.noreply.github.com>
2023-02-16 11:11:30 +0200

    Improve logs

--- app.ts
+++ app.ts
@@ -26,33 +26,39 @@ const getPostHtml = async (url: string): Promise<string> => {
         return value.replace('www', 'm')
     }
 
-    const formatedUrl = formatPostUrl(url);
+    try {
+        const formatedUrl = formatPostUrl(url);
 
-    const { data } = await axios.get(formatedUrl,
-        {
-            "headers": {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "accept-language": "en-AU,en-US;q=0.9,en;q=0.8,uk;q=0.7",
-                "cache-control": "no-cache",
-                "pragma": "no-cache",
-                "sec-ch-ua": "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": "\"macOS\"",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "none",
-                "sec-fetch-user": "?1",
-                "upgrade-insecure-requests": "1",
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-            },
+        const { data } = await axios.get(formatedUrl,
+            {
+                withCredentials: true,
+                "headers": {
+                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                    "accept-language": "en-AU,en-US;q=0.9,en;q=0.8,uk;q=0.7",
+                    "cache-control": "no-cache",
+                    "pragma": "no-cache",
+                    "sec-ch-ua": "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-ch-ua-platform": "\"macOS\"",
+                    "sec-fetch-dest": "document",
+                    "sec-fetch-mode": "navigate",
+                    "sec-fetch-site": "none",
+                    "sec-fetch-user": "?1",
+                    "upgrade-insecure-requests": "1",
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+                },
+            });
 
-        });
+        return data;
+    } catch(error) {
+        console.error(error);
 
-    return data;
+        throw error;
+    }
 }
 
 const extractImage = (html: string) => {
-    const matches = /<meta name="twitter:image" content="(.*?)" \/>/g.exec(html)
+    const matches = /<meta property="og:image" content="(.*?)" \/>/gm.exec(html)
 
     return Promise.resolve(matches ? unescape(matches[1]) : null)
 }
@@ -65,9 +71,13 @@ const getImage = async (url: string) => {
 export const handler: APIGatewayProxyHandler = async (event, context, callback) => {
     const { date, resource } = await gesSatisticst();
 
+    console.debug('hanlder', resource, date);
+
     const postHtml = await getPostHtml(resource);
 
     const image = await extractImage(postHtml)
+
+    console.debug('image', image);
 
     if(!image) {
         return {
@@ -76,18 +86,12 @@ export const handler: APIGatewayProxyHandler = async (event, context, callback) 
         }
     }
 
-    console.log(image);
-    //     app.client.chat.postMessage({
-    //         text: "",
-    // channel:"",
-    // attachments:[]
-    //     })
     const result = await app.client.files.uploadV2({
         channel_id: process.env.SLACK_CHANNEL_ID!,
 
         file: await getImage(image),
         title: `The total combat losses of the enemy from 24.02.22 to ${dateformat(date, 'dd.mm.yy')} were approximately`,
-        filename: "1.png",
+        filename: `enemy-losses-${date}.png`
     });
 
     return {
